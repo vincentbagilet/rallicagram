b049d71071e22302e24788ae01f59b21e7f228d3
Vincent Bagilet <vincent.bagilet@columbia.edu>
2023-05-05 18:37:56 +0200

    First version rallicagram_search

--- R/rallicagram_search.R
+++ R/rallicagram_search.R
@@ -3,51 +3,71 @@
 #'
 #' @param keyword A character string. Keyword to search.
 #' @param corpus A character string. The corpus to search. Takes the following
-#' values: "presse" or "press" for historical press, "livres" or "books" for
-#' Gallica books, "lemonde" for Le Monde newspaper articles.
+#' values: "press" for historical press, "books" for Gallica books,
+#' "lemonde" for Le Monde newspaper articles.
 #' @param begin A number. Starting year.
 #' @param end A number. End year.
+#' @param frequency A number. End year.
 #'
-#' @returns A numeric vector.
+#' @returns A tibble.
 #'
 #' @export
 #' @examples
 #' rallicagram_search("président")
 rallicagram_search <- function(keyword,
-                               corpus="presse",
+                               corpus="press",
                                begin=1789,
-                               end=1950) {
-
-  keyword_clean <- str_replace(tolower(keyword)," ","%20")
-
-  # clean corpus name
-  if (corpus %in% c("presse", "press")) {
-    corpus_clean <- "presse"
-  } else if (corpus %in% c("books", "livres")) {
-    corpus_clean <- "livres"
-  } else if (corpus == "lemonde") {
-    corpus_clean <- "lemonde"
-  } else {
-    stop("Invalid corpus name")
-  }
+                               end=1950,
+                               frequency="monthly") {
 
   if (!is.numeric(as.numeric(begin)) | !is.numeric(as.numeric(end))) {
-    stop("'begin' and 'end' should be numeric")
+    stop("'begin' and 'end' should be numeric", call. = FALSE)
   }
 
-  api_call <- paste(
-    "https://shiny.ens-paris-saclay.fr/guni/query?corpus=",
-    corpus,
-    "&mot=",
-    keyword_clean,
-    "&from=",
-    begin,
-    "&to=",
-    end,
-    sep="")
+  if (!is.character(keyword)) {
+    stop("'keyword' should be a character string", call. = FALSE)
+  }
 
-  api_call |>
-    read_csv()
+  #translations
+  resolution <- ifelse(frequency == "yearly", "annee",
+                       ifelse(frequency == "monthly", "mois",
+                       ifelse(frequency == "daily", "jour",
+                          stop("Invalid frequency", call. = FALSE))))
 
-  # return(read.csv(api_call))
+  corpus_french <- ifelse(corpus == "press", "presse",
+                          ifelse(corpus == "books", "livres",
+                          ifelse(corpus == "lemonde", "lemonde",
+                            stop("Invalid corpus name", call. = FALSE))))
+
+  keyword_clean <- sub(" ","%20", tolower(keyword))
+
+  output <- paste("https://shiny.ens-paris-saclay.fr/guni/query?corpus=",
+                  corpus_french,
+                  "&mot=",
+                  keyword_clean,
+                  "&from=",
+                  begin,
+                  "&to=",
+                  end,
+                  "&resolution=",
+                  resolution,
+                  sep="") |>
+    utils::read.csv() |>
+    #tidy
+    dplyr::as_tibble() |>
+    dplyr::rename(keyword = "gram", year = "annee") |>
+    dplyr::rename(
+      tidyselect::any_of(c(month = "mois", day = "jour"))
+    ) |>
+    dplyr::mutate(
+      source = corpus,
+      frequency = frequency,
+      month = ifelse(frequency == "yearly", 01, month),
+      day = ifelse(frequency != "daily", 01, day),
+      date = as.Date(paste(year, month, day, sep = "-"))
+    ) |>
+    dplyr::select(date, keyword, "n", total, source, frequency)
+
+  return(output)
 }
+

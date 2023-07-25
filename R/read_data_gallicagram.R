a5c80a4fc04b245c237acd9488c93da7c1607cf9
Vincent Bagilet <vincent.bagilet@columbia.edu>
2023-07-25 11:53:11 +0200

    Timeout 5000s

--- R/read_data_gallicagram.R
+++ R/read_data_gallicagram.R
@@ -6,11 +6,15 @@
 #'
 #' @param url The url to pass to the Gallicagram API
 #'
+#' @details
+#' The timeout is set to 5000s to handle queries that take Gallicagram
+#' a long time to process.
+#'
 #' @returns A tibble containing the raw data from the Gallicagram API.
 #'
 read_data_gallicagram <- function(url) {
 
-  options(timeout = 500)
+  options(timeout = 5000)
 
   output <- url |>
     utils::read.csv() |>
